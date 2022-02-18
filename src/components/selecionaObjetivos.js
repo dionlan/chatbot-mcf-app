f09b680e1058d7dc8802419282f13b348c4ddb1d
dionlan <dionlan.alves@gmail.com>
2022-02-18 16:38:00 -0300

    Atualização na apresentação da lista dos objetivos financeiros para exibir a nota final correspondente com os objetivos selecionados.

--- src/components/selecionaObjetivos.js
+++ src/components/selecionaObjetivos.js
@@ -1,41 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from 'primereact/button';
 import ObjetivosFinanceiros from "../utils/objetivosFinanceiros";
 import './components.css'
-import { Checkbox } from "primereact/checkbox";
-import { MultiSelect } from 'primereact/multiselect';
-
-const getFormattedNota = (nota) => `${nota.toFixed(2)}`;
 
 function SelecionaObjetivos (props) {
   const [checkedState, setCheckedState] = useState(
     new Array(ObjetivosFinanceiros.length).fill(false)
   );
 
-  const [total, setTotal] = useState(0);
+  const [listaObjetivos, setListaObjetivos] = useState([]);
+  const [notaFinal, setNotaFinal] = useState([]);
 
-  const [objetivos, setObjetivos] = useState([]);
   let state = [];
-  const handleOnChange = ( position ) => {
+
+  const handleOnChange = ( position) => {
     const updatedCheckedState = checkedState.map((item, index) =>
       index === position ? !item : item,
-      
     );
-    
+    setCheckedState(updatedCheckedState);
+
     const totalObjetivos = updatedCheckedState.reduce(
       (obj, currentState, index) => {
         if (currentState === true) {
-          console.log(index)
-          return state += ObjetivosFinanceiros[index].objetivo + ' '
+          return obj += ObjetivosFinanceiros[index].objetivo;
         }
         return obj
-      }
+      },
+      ''
     );
-    
-    setCheckedState(updatedCheckedState);
-    setObjetivos(totalObjetivos)
 
-    const totalNota = updatedCheckedState.reduce(
+    setListaObjetivos(totalObjetivos)
+
+    const notaFinalAtualizada = updatedCheckedState.reduce(
       (obj, currentState, index) => {
         if (currentState === true) {
           return obj + ObjetivosFinanceiros[index].nota;
@@ -43,76 +39,40 @@ function SelecionaObjetivos (props) {
         return obj;
       },
       0
-    ); 
-    setTotal(totalNota);
+    );
+    setNotaFinal(notaFinalAtualizada);
+  
   };
-
-  const [selecionado, setSelecionado] = useState ([])
-
-  const [checked, setChecked] = useState([]);
-
-  const handleCheck = (event) => {
-    var updatedList = [...checked];
-    if (event.target.checked) {
-      updatedList = [...checked, event.target.value];
-    } else {
-      updatedList.splice(checked.indexOf(event.target.value), 1);
-    }
-    setChecked(updatedList);
-  };
-
-  const isChecked = (item) =>
-  checked.includes(item) ? true: false;
-  var checkedItems = checked.length
-  ? checked.reduce((total, item) => {
-      return total + ", " + item;
-    })
-  : "";
-console.log(selecionado)
-const listaObjetivos = selecionado.map((objetivos) => {
-  return ' '+objetivos.objetivo;
-});
-
-  function onCityChange(e) {
-    let selectedObjetivos= [...selecionado];
-
-    if (e.checked)
-      selectedObjetivos.push(e.value);
-    else
-      selectedObjetivos.splice(selectedObjetivos.indexOf(e.value), 1);
-
-    setSelecionado({ selecionado: selectedObjetivos });
-}
   return (
     <div>
       <h3>Objetivos Financeiros Imediatos</h3>
-      {ObjetivosFinanceiros.map((objetivo) => {
-    
-            <div key={objetivo.objetivo} className="field-checkbox">
-              <Checkbox inputId={objetivo.objetivo} name="objetivo" value={objetivo} onChange={onCityChange} checked={e => setChecked( {checked: e.checked }) } 
-              options={ObjetivosFinanceiros} />
-              <label htmlFor={objetivo.objetivo}>{objetivo.objetivo}</label>
-            </div>
-          
-      })}
-
-      <div>
-        <div className="multiselect-demo">  
-          <Checkbox value={selecionado} 
-              options={ObjetivosFinanceiros} 
-              onChange={ (e) => setSelecionado( e.value)} 
-              optionLabel="objetivo" 
-              placeholder="Selecione" />
+      <ul>
+        {ObjetivosFinanceiros.map(({ objetivo }, index) => {
+          return (
+            <li key={index}>
+                  <input
+                    type={'checkbox'}
+                    id={`custom-checkbox-${index}`}
+                    objetivo={objetivo}
+                    value={objetivo}
+                    checked={checkedState[index]}
+                    onChange={() => handleOnChange(index)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>{objetivo}</label>
+            </li>
+          );
+        })}
+        <div>
+          <label><strong>Objetivos Selecionados:  </strong>{listaObjetivos}</label>
         </div>
-      {`listaObjetivos: ${listaObjetivos}`}
-      </div>
-      <br/>
-  
-        
+        <div>
+          <label><strong>Nota Final: </strong>{notaFinal}</label>
+        </div>
+      </ul>
       <br/>
       <div>
         <Button className="p-button-success p-button-sm" onClick={() => props.triggerNextStep({id: 'objetivosFinanceirosImediatos', 
-            value: objetivos.selecionados, trigger: 'resumo' })}>
+            value: listaObjetivos, trigger: 'resumo' })}>
             Prosseguir
         </Button>
       </div>
