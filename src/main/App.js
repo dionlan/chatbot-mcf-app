2d416f5ec6d53d632f31d3e8732181c66badb6b3
dionlan <dionlan.alves@gmail.com>
2022-02-19 17:44:21 -0300

    Criação do componente navbar para ser implementado a navegação entre as rotas. Instalação da lib bootswatch.

--- src/main/App.js
+++ src/main/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Rotas from './rotas'
+import "bootswatch/dist/minty/bootstrap.min.css"; 
 import 'primereact/resources/themes/lara-light-indigo/theme.css';  //theme
 import 'primereact/resources/primereact.min.css';                  //core css
 import 'primeicons/primeicons.css';                                //icons
+import Navbar from '../components/navbar';
 
 function App() {
   return (
-    <Rotas />
+    <>
+      <Navbar />
+        <div className='container'>
+          <Rotas />
+        </div>
+    </>
   );
 }
 export default App;
