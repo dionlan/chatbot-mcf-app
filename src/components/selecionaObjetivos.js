a6710c0bcf8bb20781049867c909183a18c9c1e1
dionlan <dionlan.alves@gmail.com>
2022-02-18 23:51:56 -0300

    Ajuste no CSS do chat para melhorar a usabilidade na marcação dos objetivos financeiros imediatos. =]

--- src/components/selecionaObjetivos.js
+++ src/components/selecionaObjetivos.js
@@ -10,7 +10,9 @@ function SelecionaObjetivos (props) {
     new Array(ObjetivosFinanceiros.length).fill(false)
   );
 
-  const [state, setState] = useState({ outros: ''})
+  const [state, setState] = useState({ 
+    outros: []
+  })
 
   const handleInputChange = (event) => {
     setState((prevProps) => ({
@@ -53,22 +55,23 @@ function SelecionaObjetivos (props) {
   };
   return (
     <div>
+      <div className="card">
       <h3>Objetivos Financeiros Imediatos</h3>
       <ul>
         {ObjetivosFinanceiros.map(({ objetivo, nota }, index) => {
           return (
             <li key={index}>
               {objetivo !== 'Outros' ?
-                <>
-                  <Checkbox
-                    type={'checkbox'}
-                    id={`custom-checkbox-${index}`}
-                    value={objetivo}
-                    checked={checkedState[index]}
-                    onChange={() => handleOnChange(index)}
-                  />
-                  <label htmlFor={`custom-checkbox-${index}`}>{objetivo}</label>
-                </>
+              <>
+                <Checkbox
+                  type={'checkbox'}
+                  id={`custom-checkbox-${index}`}
+                  value={objetivo}
+                  checked={checkedState[index]}
+                  onChange={() => handleOnChange(index)}
+                />
+                <label htmlFor={`custom-checkbox-${index}`}>{objetivo}</label>
+              </>
               : 
               <InputText type="text" name="outros" value={state.outros} className="p-inputtext-sm" onChange={handleInputChange} placeholder="Outros objetivos" />
               }
@@ -84,12 +87,15 @@ function SelecionaObjetivos (props) {
       </ul>
       <br/>
       <div>
-        <Button className="p-button-success p-button-sm" onClick={() => props.triggerNextStep({id: 'objetivosFinanceirosImediatos', 
-            value: listaObjetivos + state.outros, trigger: 'resumo' })}>
+        <Button className="p-button-success p-button-sm" onClick={() => props.triggerNextStep({value: listaObjetivos + state.outros, 
+                                                                                               trigger: 'resumo' })}>
             Prosseguir
         </Button>
       </div>
     </div>
+
+    </div>
   );
+  
 }
 export default SelecionaObjetivos
