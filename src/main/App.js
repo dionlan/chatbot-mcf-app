c3de9bb8e4d56de6af0930b3b3dc34fa17760d46
dionlan <dionlan.alves@gmail.com>
2022-03-01 01:41:28 -0300

    Atualização para sobrescrever o CSS da tabela dos resultados da análise financeira oriunda do primereact.

--- src/main/App.js
+++ src/main/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Rotas from './rotas'
 import Navbar from '../components/navbar';
 
-/*import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css'; */
+import 'primeicons/primeicons.css';
 import 'bootswatch/dist/minty/bootstrap.min.css';
 import '../styles.css';
 
