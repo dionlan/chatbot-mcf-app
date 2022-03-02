ea7b204dcf79c4dacc6a5a5612fc3afdfbe7e14c
dionlan <dionlan.alves@gmail.com>
2022-03-02 02:03:01 -0300

    Início da referatoração nas etapas de cada questão do chatbot, com vistas à adequar os ids das questões com o modelo relacionado do banco.

--- src/utils/objetivosFinanceiros.js
+++ src/utils/objetivosFinanceiros.js
@@ -1,53 +1,66 @@
 const ObjetivosFinanceiros = [
     {
+      id: 1,
       objetivo: "Aumentar as receitas",
       nota: 1.21
     },
     {
+      id: 2,
       objetivo: "Aprender a gerenciar o meu orçamento, receitas e despesas",
       nota: 2.62
     },
     {
+      id: 3,
       objetivo: "Consumir de forma mais consciente",
       nota: 2.92
     },
     {
+      id: 4,
       objetivo: "Quitar as minhas dívidas",
       nota: 3.51
     },
     {
+      id: 5,
       objetivo: "Definir e ter mais clareza dos meus objetivos financeiros",
       nota: 3.51
     },
     {
+      id: 6,
       objetivo: "Montar minha reserva de segurança",
       nota: 1.99
     },
     {
+      id: 7,
       objetivo: "Juntar dinheiro para a minha aposentadoria",
       nota: 3.39
     },
     {
+      id: 8,
       objetivo: "Investir melhor para alcançar os meus objetivos",
       nota: 2.83
     },
     {
+      id: 9,
       objetivo: "Estar mais protegido contra acidentes e imprevistos",
       nota: 2.41
     },
     {
+      id: 10,
       objetivo: "Melhorar a minha educação financeira",
       nota: 1.89
     },
     {
+      id: 11,
       objetivo: "Melhorar meus pensamentos e emoções relacionadas ao dinheiro",
       nota: 2.11
     },
     {
+      id: 12,
       objetivo: "Comprar algum bem pessoal ou imóvel",
       nota: 2.23
     },
     {
+      id: 13,
       objetivo: "Outros",
       nota: 1.45
     }
