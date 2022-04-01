83397f31e59015427d9fb5c48b7cd0616571d761
dionlan <dionlan.alves@gmail.com>
2022-04-01 20:43:53 -0300

    Adição da biblioteca React Score Indicator para apresentar a escala usual de 0 até 1000 ao usuário com sua situação da saúde financeira entre (risco, alerta, mediano, favorável ou bem estar financeiro).

--- src/components/preDiagnostico.js
+++ src/components/preDiagnostico.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PreDiagnosticoService from '../service/preDiagnosticoService'
+import { Message } from 'primereact/message';
+import ReactStoreIndicator from 'react-score-indicator'
 
 const PreDiagnostico = (props) => {
   const service = new PreDiagnosticoService();
@@ -22,11 +24,32 @@ const PreDiagnostico = (props) => {
   console.log('PRÉVIA DO DIAGNÓSTICO: ', previaDiagnostico)
   
   return (
-    <div>
-      <h5>Muito obrigado, {personInput.name}!</h5> 
-      <p>Sua nota prévia: <strong> {JSON.stringify(previaDiagnostico.finalNote)} </strong></p>
-      <p>Sua classificação prévia é considerada como: <strong> {JSON.stringify(previaDiagnostico.classification)} </strong></p>
-    </div>
+    <>
+      <div>
+        <h5>Muito obrigado, {personInput.name}!</h5> 
+        <p>Sua nota parcial foi de <strong>{JSON.stringify(previaDiagnostico.finalNote)}</strong> pontos. </p>
+        
+        Isso significa que seu diagnóstico atual é classificado como: <strong> {JSON.stringify(previaDiagnostico.classification)} </strong>
+        <br/>
+        <br/>
+      </div>
+      
+      <div>
+      <ReactStoreIndicator 
+        value={previaDiagnostico.finalNote}
+        textStyle={{bottom: '45px'}}
+        maxValue={1000}
+        lineWidth={40}
+        lineGap={2} 
+        fadedOpacity={20}
+        borderWidth={100}
+        maxAngle={180}/>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: 'center'}}>
+        <Message severity="warn" text={previaDiagnostico.classification}></Message>
+      </div>
+    </>
   )
 }
 export default PreDiagnostico;
