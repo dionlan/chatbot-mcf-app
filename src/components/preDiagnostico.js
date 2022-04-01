d715deaccd987f17466e14f561df5c96ecacd27d
dionlan <dionlan.alves@gmail.com>
2022-04-01 14:02:53 -0300

    Ajusta final de ciclo concluído até a última etapa que apresenta a prévia do diagnóstico financeiro. Falta refatorar alguns pontos e criar os serviços de notificações para o usuário criado e os especialistas financeiros.

--- src/components/preDiagnostico.js
+++ src/components/preDiagnostico.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import PreDiagnosticoService from '../service/preDiagnosticoService'
-import { useContext } from "react";
 
 const PreDiagnostico = (props) => {
   const service = new PreDiagnosticoService();
-  const [preDiagnostico, setPrediagnostico] = useState([]);
+  const [previaDiagnostico, setPreviaDiagnostico] = useState([]);
+  
   const personInput = props.respostas
-
   useEffect(() => {
-    console.log('PROPS USERCONTEXT USER: ', personInput)
     service.buscarResultadoPrevio(personInput.id)
     .then(response => {
       const lista = response.data
         if(lista.length < 1){
             console.log('Nenhum resultado encontrado.')
         }
-      setPrediagnostico(preDiagnostico => lista)
+        setPreviaDiagnostico(preDiagnostico => response.data)
       props.triggerNextStep({id: 'preDiagnostico', message:'pre_diagnostico', trigger: 'finaliza' })
     }).catch(error => {
       console.log('ERRO!', error)
     })
   }, []) 
-
+  console.log('PRÉVIA DO DIAGNÓSTICO: ', previaDiagnostico)
+  
   return (
-    <>
-    {preDiagnostico}
-    </>
+    <div>
+      <h5>Muito obrigado, {personInput.name}!</h5> 
+      <p>Sua nota prévia: <strong> {JSON.stringify(previaDiagnostico.finalNote)} </strong></p>
+      <p>Sua classificação prévia é considerada como: <strong> {JSON.stringify(previaDiagnostico.classification)} </strong></p>
+    </div>
   )
 }
 export default PreDiagnostico;
