640c7be9aaa2ded2b2832e1d51a4e481897c65b6
dionlan <dionlan.alves@gmail.com>
2022-04-22 07:37:04 -0300

    Deploy Heroku novos testes.

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -10,7 +10,7 @@ function Rotas(){
     return(
         <Router>
             <Routes>
-                <Route path='/' element={ <Chatbot/> }/>
+                <Route path='/' element={ <Novo/> }/>
                 <Route path='/chat' element={ <Chatbot /> } />
                 <Route path='/chat/novo' element={ <Novo /> } />
                 <Route path='/teste/testeCheckbox' element={ <TesteCheckbox /> }/>
